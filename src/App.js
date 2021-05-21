7306061197d201e6fffcca08b20af6cc183a807b
MartianTolu <tolulope.adedoyin@deimos.co.za>
2021-05-21 08:34:15 +0100

    Signin and SignUp page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Homepage from './pages/homepage/homepage.component'
 import { Switch, Route } from 'react-router-dom'
 import ShopPage from '../src/pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignUpPage from '../src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
